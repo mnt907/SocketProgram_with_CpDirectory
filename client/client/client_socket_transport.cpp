ffd45cc0b5bf9b5491036c1a6753679b428a234b
mnt907 <mnt907@naver.com>
2020-08-27 18:58:08 +0900

    계속구동하는 서버

--- client/client/client_socket_transport.cpp
+++ client/client/client_socket_transport.cpp
@@ -1,3 +1,4 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <sys/types.h>
@@ -7,7 +8,7 @@
 #include <filesystem>
 #include <Windows.h>
 #include <fstream>
-#include <errno.h>
+#include <WS2tcpip.h>
 #else
 #include <sys/socket.h>
 #include <dirent.h>
@@ -25,9 +26,25 @@
 #define PORT 5555
 #define PACKET_SIZE 1400
 #define SERVER_IP "127.0.0.1"
+#define COUNT 1
 
 struct FileInfomation
 {
+    FileInfomation()
+        : file_size (0)
+        , is_directory(false)
+    {
+        memset(file_path, 0, sizeof(file_path));
+    }
+
+    FileInfomation(const char* file_name, int file_size, bool is_dir)
+        : file_size(file_size)
+        , is_directory(is_dir)
+    {
+        memset(file_path, 0, sizeof(file_path));
+        memcpy(file_path, file_name, sizeof(file_name));
+    }
+
     char file_path[256];
     __int64 file_size;
     bool is_directory;
@@ -75,7 +92,7 @@ namespace
 #endif //_WIN32
     }
 
-    bool IsExistDiretory(const std::string& dir_path)
+    bool IsExistDirectory(const std::string& dir_path)
     {
 #ifdef _WIN32
         if (fs::is_directory(fs::path(dir_path)))
@@ -95,73 +112,110 @@ namespace
         return false;
     }
 
-    bool SendFileData(const std::string& src_path, SOCKET& socket, struct FileInfomation file_info)
+    bool SendFileData(const std::string& src_path, const SOCKET& socket, FileInfomation& file_info)
     {
-        FILE* src_fp = NULL;
-        src_fp = fopen(src_path.c_str(), "rb");
-        if (file_info.is_directory != true)
+        if (file_info.is_directory)
         {
-            if (src_fp == NULL)
+            int sendlen = send(socket, (char*)&file_info, sizeof(file_info), 0);
+            if (sendlen != sizeof(file_info)) 
             {
-                std::cout << "can't create read_filepointer" << std::endl;
+                std::cout << "can't send file_infomation" << std::endl;
                 return false;
             }
+            return true;
         }
-        else 
+
+        errno_t err;
+        FILE* src_fp = NULL;
+        err = fopen_s(&src_fp, src_path.c_str(), "rb");
+        if (err != 0)
         {
-            file_info.file_size = 0;
-            send(socket, (char*)&file_info, sizeof(file_info), 0);
+            std::cout << "can't create read_filepointer" << std::endl;
             return false;
         }
 
         _fseeki64(src_fp, (__int64)0, SEEK_END);
-        const __int64 file_size = _ftelli64(src_fp);
-        if (file_size < 0)
+        const __int64 FILE_SIZE = _ftelli64(src_fp);
+        if (FILE_SIZE < 0)
         {
-            printf("can't get a file's offset. errno(%d, %s)\r\n", errno, strerror(errno));
+            std::cout << "can't get a file's offset" << std::endl;
             return false;
         }
         rewind(src_fp);
 
-        file_info.file_size = file_size;
+        file_info.file_size = FILE_SIZE;
 
         char send_buf[PACKET_SIZE] = { 0 };
-        //send struct 
-        send(socket, (char*)&file_info, sizeof(file_info), 0);
+        int sendlen = send(socket, (char*)&file_info, sizeof(file_info), 0);
+        if (sendlen != sizeof(file_info))
+        {
+            std::cout << "can't send file_infomation" << std::endl;
+            return false;
+        }
+            
         std::cout << "file_path : " << file_info.file_path << std::endl;
         std::cout << "file_size : " << file_info.file_size << std::endl;
         std::cout << "is_directory : " << file_info.is_directory << std::endl;
 
-        const int copy_number = file_size / PACKET_SIZE;
-        const int last_file_size = file_size % PACKET_SIZE;
+        const int COPY_NUMBER = (int)FILE_SIZE / PACKET_SIZE;
+        const int LAST_FILE_SIZE = (int)FILE_SIZE % PACKET_SIZE;
 
-        for (int i = 0; i < copy_number; i++)
+        for (int i = 0; i < COPY_NUMBER; i++)
         {
             memset(send_buf, 0, PACKET_SIZE);
-            fread(send_buf, PACKET_SIZE, 1, src_fp);
-            send(socket, send_buf, PACKET_SIZE, 0);
+            int readlen = fread(send_buf, PACKET_SIZE, COUNT, src_fp);
+            if (readlen != COUNT)
+            {
+                std::cout << "can't read file_data" << std::endl;
+                return false;
+            }
+     
+            int sendlen = send(socket, send_buf, PACKET_SIZE, 0);
+            if (sendlen != PACKET_SIZE)
+            {
+                std::cout << "can't send file_data" << std::endl;
+                return false;
+            }
+               
             memset(send_buf, 0, PACKET_SIZE);
-            
         }
         Sleep(1);
-        if (last_file_size > 0)
+
+        if (LAST_FILE_SIZE > 0)
         {
-            memset(send_buf, 0, last_file_size);
-            fread(send_buf, last_file_size, 1, src_fp);
-            send(socket, send_buf, last_file_size, 0);
-            memset(send_buf, 0, last_file_size);
+            memset(send_buf, 0, LAST_FILE_SIZE);
+            int readlen = fread(send_buf, LAST_FILE_SIZE, COUNT, src_fp);
+            if (readlen != COUNT)
+            {
+                std::cout << "can't read file_data" << std::endl;
+                return false;
+            }
+                
+
+            int sendlen = send(socket, send_buf, LAST_FILE_SIZE, 0);
+            if (sendlen != LAST_FILE_SIZE)
+            {
+                std::cout << "can't send file_data" << std::endl;
+                return false;
+            }
+
+            memset(send_buf, 0, LAST_FILE_SIZE);
         }
         fclose(src_fp);
+        return true;
     }
 
     bool DirectoryCopy(const std::string& src_path, SOCKET& socket)
     {
 #ifdef _WIN32
-        const int file_num = CountFiles(src_path);
-
         char send_buf[PACKET_SIZE] = { 0 };
-        sprintf(send_buf, "%d", file_num);
-        send(socket, send_buf, PACKET_SIZE, 0);
+        sprintf_s(send_buf, "%d", CountFiles(src_path));
+        int sendlen = send(socket, send_buf, PACKET_SIZE, 0);
+        if (sendlen != PACKET_SIZE)
+        {
+            std::cout << "can't send count_files" << std::endl;
+            return false;
+        }
 
         auto dir = fs::recursive_directory_iterator(fs::path(src_path));
 
@@ -171,17 +225,24 @@ namespace
             file_path.assign(file_path.c_str(), src_path.length(), file_path.length());
             std::cout << "file_path : " << file_path << std::endl;
 
-            const std::string src_file_path = src_path + file_path;
+            const std::string SRC_FILE_PATH = src_path + file_path;
 
-            struct FileInfomation file_info;
-            strcpy(file_info.file_path, file_path.c_str());
+            FileInfomation file_info;
 
-            if (IsExistDiretory(src_file_path))
+            strncpy_s(file_info.file_path, file_path.c_str()
+                , (sizeof(file_info.file_path) < file_path.length()) ?
+                sizeof(file_info.file_path) - 1 : file_path.length());
+
+            if (IsExistDirectory(SRC_FILE_PATH))
                 file_info.is_directory = true;
             else
                 file_info.is_directory = false;
                
-            SendFileData(src_file_path, socket, file_info);
+            if (SendFileData(SRC_FILE_PATH, socket, file_info) == false)
+            {
+                std::cout << "fail to send file data " << file_path << std::endl;
+                return false;
+            }          
         }
         return true;
 #else
@@ -247,39 +308,92 @@ namespace
 
 #endif //_WIN32
     }
+    std::string InputPath()
+    {
+        std::string user_input_path;
+        std::cin >> user_input_path;
+        std::cout << user_input_path << "를 입력하였음" << std::endl;
+        return user_input_path;
+    }
 }
 
 int main()
 {
-    WSADATA wsa_data;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) == -1)
+    for (int i = 0; i < 1000; ++i)
     {
-        std::cout << "WSAStartup errno : " << errno << std::endl;
+        WSADATA wsa_data;
+        if (WSAStartup(MAKEWORD(2, 2), &wsa_data) == -1)
+        {
+            std::cout << "WSAStartup errno : " << errno << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        SOCKET client_socket;
+        client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (client_socket == -1)
+        {
+            std::cout << "socket errno : " << errno << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        SOCKADDR_IN client_addr;
+        client_addr.sin_family = AF_INET;
+        client_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+        client_addr.sin_port = htons(PORT);
+
+        if (connect(client_socket, (SOCKADDR*)&client_addr, sizeof(client_addr)) == -1)
+        {
+            std::cout << "connect errno : " << errno << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        char src_path[PACKET_SIZE] = { 0 };
+        char dst_path[PACKET_SIZE] = { 0 };
+
+        std::cout << "src_path입력 : ";
+        std::string input_path = InputPath();
+        strncpy_s(src_path, input_path.c_str()
+            , (sizeof(src_path) < input_path.length()) ?
+            sizeof(src_path) - 1 : input_path.length());
+
+        std::cout << std::endl;
+
+        std::cout << "dst_path입력 : ";
+        input_path.clear();
+        input_path = InputPath();
+        strncpy_s(dst_path, input_path.c_str()
+            , (sizeof(dst_path) < input_path.length()) ?
+            sizeof(dst_path) - 1 : input_path.length());
+
+        std::cout << std::endl;
+
+        int sendlen = send(client_socket, dst_path, sizeof(dst_path), 0);
+        if (sendlen != sizeof(dst_path)) 
+        {
+            std::cout << "can't send dst_path" << std::endl;
+            return EXIT_FAILURE;
+        }
+
+        if (IsExistDirectory(src_path) == false) 
+        {
+            std::cout << "can't check Directory" << std::endl;
+            return EXIT_FAILURE;
+        }
+            
+
+        if (DirectoryCopy(src_path, client_socket) == false) 
+        {
+            std::cout << "fail to copy directory" << std::endl;
+            return EXIT_FAILURE;
+        }
+            
+        closesocket(client_socket);
+
+        WSACleanup();
+
     }
 
-    SOCKET client_socket;
-    client_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (client_socket == -1)
-        std::cout << "socket errno : " << errno << std::endl;
-
-    SOCKADDR_IN client_addr;
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-    client_addr.sin_port = htons(PORT);
-
-    if (connect(client_socket, (SOCKADDR*)&client_addr, sizeof(client_addr)) == -1)
-    {
-        std::cout << "connect errno : " << errno << std::endl;
-    }
-
-    const std::string src_path = "C:/Users/mnt/Desktop/empty_directory";
-
-    if (IsExistDiretory(src_path) == false)
-        return false;
-
-    DirectoryCopy(src_path, client_socket);
-
-    closesocket(client_socket);
-
-    WSACleanup();
+    return 0;
 }
+
+
--- server/server/server_socket_transport.cpp
+++ server/server/server_socket_transport.cpp
@@ -3,6 +3,11 @@
 #ifdef _WIN32
 #include <WinSock2.h>
 #include <filesystem>
+#include <conio.h>
+#include <conio.h>
+#include <cassert>
+#include <conio.h>
+#include <stdio.h>
 #else
 #include <sys/socket.h>
 
@@ -13,9 +18,17 @@
 #define PORT 5555
 #define PACKET_SIZE 1400
 #define LISTEN_BACKLOG 50
+#define COUNT 1
 
 struct FileInfomation
 {
+    FileInfomation()
+        : file_size(0)
+        , is_directory(false)
+    {
+        memset(file_path, 0, sizeof(file_path));
+    }
+
     char file_path[256];
     __int64 file_size;
     bool is_directory;
@@ -23,38 +36,59 @@ struct FileInfomation
 
 namespace
 {
-    bool RecvFileData(const std::string& dst_path, SOCKET& socket, struct FileInfomation file_info)
+    bool RecvFileData(const std::string& dst_path, SOCKET& socket, FileInfomation& file_info)
     {
+        errno_t err;
         FILE* dst_fp = NULL;
-        dst_fp = fopen(dst_path.c_str(), "wb");
-        if (dst_fp == NULL)
+        err = fopen_s(&dst_fp, dst_path.c_str(), "wb");
+        if (err != 0)
         {
             std::cout << "can't create write_filepointer" << std::endl;
             return false;
         }
 
-        const __int64 file_size = file_info.file_size;
-        std::cout << file_size << std::endl;
+        const __int64 FILE_SIZE = file_info.file_size;
+        std::cout << FILE_SIZE << std::endl;
 
-        const int copy_number = file_size / PACKET_SIZE;
-        const int last_file_size = file_size % PACKET_SIZE;
+        const int COPY_NUMBER = (int)FILE_SIZE / PACKET_SIZE;
+        const int LAST_FILE_SIZE = (int)FILE_SIZE % PACKET_SIZE;
 
         char recv_buf[PACKET_SIZE] = { 0 };
 
-        for (int i = 0; i < copy_number; i++)
+        for (int i = 0; i < COPY_NUMBER; i++)
         {
             memset(recv_buf, 0, PACKET_SIZE);
-            recv(socket, recv_buf, PACKET_SIZE, 0);
-            fwrite(recv_buf, PACKET_SIZE, 1, dst_fp);
+            int recvlen = recv(socket, recv_buf, PACKET_SIZE, 0);
+            if (recvlen != PACKET_SIZE)
+            {
+                std::cout << "can't recv file_data" << std::endl;
+                return false;
+            }
+            int writelen = fwrite(recv_buf, PACKET_SIZE, COUNT, dst_fp);
+            if (writelen != COUNT)
+            {
+                std::cout << "can't write file_data" << std::endl;
+                return false;
+            }
             memset(recv_buf, 0, PACKET_SIZE);
         }
 
-        if (last_file_size > 0)
+        if (LAST_FILE_SIZE > 0)
         {
-            memset(recv_buf, 0, last_file_size);
-            recv(socket, recv_buf, last_file_size, 0);
-            fwrite(recv_buf, last_file_size, 1, dst_fp);
-            memset(recv_buf, 0, last_file_size);
+            memset(recv_buf, 0, LAST_FILE_SIZE);
+            int recvlen = recv(socket, recv_buf, LAST_FILE_SIZE, 0);
+            if (recvlen != LAST_FILE_SIZE)
+            {
+                std::cout << "can't recv file_data" << std::endl;
+                return false;
+            }
+            int writelen = fwrite(recv_buf, LAST_FILE_SIZE, COUNT, dst_fp);
+            if (writelen != COUNT)
+            {
+                std::cout << "can't write file_data" << std::endl;
+                return false;
+            }
+            memset(recv_buf, 0, LAST_FILE_SIZE);
         }
 
         fclose(dst_fp);
@@ -63,37 +97,53 @@ namespace
 
     bool DirectoryCopy(const std::string& dst_path, SOCKET& socket)
     {
-        struct FileInfomation file_info;
         char recv_buf[PACKET_SIZE] = { 0 };
         int recvlen = recv(socket, recv_buf, PACKET_SIZE, 0);
-        const int file_num = atoi(recv_buf);
-
-        for (int i = 0; i < file_num; i++) 
+        if (recvlen != PACKET_SIZE)
         {
-            //memset(&file_info, 0, sizeof(file_info));
+            std::cout << "can't recv file_data" << std::endl;
+            return false;
+        }
+
+        const int FILE_NUM = atoi(recv_buf);
+        struct FileInfomation file_info;
+
+        for (int i = 0; i < FILE_NUM; i++)
+        {
             recvlen = recv(socket, (char*)&file_info, sizeof(file_info), 0);
+            if (recvlen != sizeof(file_info))
+            {
+                std::cout << "can't recv file_info" << std::endl;
+                return false;
+            }
             std::cout << "file_path : " << file_info.file_path << std::endl;
             std::cout << "file_size : " << file_info.file_size << std::endl;
             std::cout << "is_directory : " << file_info.is_directory << std::endl;
 
-            const std::string dst_file_path = dst_path + file_info.file_path;
-            std::cout << "dst_file_path : " << dst_file_path << std::endl;
+            const std::string DST_FILE_PATH = dst_path + file_info.file_path;
+            std::cout << "dst_file_path : " << DST_FILE_PATH << std::endl;
 
             if (file_info.is_directory == true)
             {
                 std::cout << "make directory" << std::endl;
                 namespace fs = std::experimental::filesystem;
-                fs::create_directory(dst_file_path);
+                //if (fs::create_directory(DST_FILE_PATH) == false)
+                //    return false;
+                fs::create_directory(DST_FILE_PATH);
             }
             else
             {
-                RecvFileData(dst_file_path, socket, file_info);
+                if(RecvFileData(DST_FILE_PATH, socket, file_info) == false)
+                {
+                    std::cout << "can't RecvFileData" << std::endl;
+                    return false;
+                }
             }
         }
         return true;
     }
 
-    bool IsExistDiretory(const std::string& dir_path)
+    bool IsExistDirectory(const std::string& dir_path)
     {
         namespace fs = std::experimental::filesystem;
         if (fs::is_directory(fs::path(dir_path)))
@@ -101,60 +151,84 @@ namespace
             std::cout << "exists Directory" << std::endl;
             return true;
         }
-
         std::cout << "not exists Directory" << std::endl;
         return false;
     }
-
-    std::string InputPath()
-    {
-        std::cout << "Enter the PATH" << std::endl;
-        std::string user_input;
-        std::cin >> user_input;
-        std::cout << user_input << std::endl;
-        return user_input;
-    }
 }
 
-bool main()
+int main()
 {
     WSADATA wsa_data;
     if (WSAStartup(MAKEWORD(2, 2), &wsa_data) == -1)
     {
         std::cout << "WSAStartup errno : " << errno << std::endl;
+        return false;
     }
 
     SOCKET server_socket;
     server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (server_socket == -1)
+    {
         std::cout << "socket errno : " << errno << std::endl;
-
+        return false;
+    }
     SOCKADDR_IN server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(PORT);
 
     if (bind(server_socket, (SOCKADDR*)&server_addr, sizeof(server_addr)) == -1)
+    {
         std::cout << "bind errno : " << errno << std::endl;
+        return false;
+    }
 
     if (listen(server_socket, LISTEN_BACKLOG) == -1)
+    {
         std::cout << "listen errno : " << errno << std::endl;
+        return false;
+    }
 
     int sockaddr_size = sizeof(SOCKADDR_IN);
     SOCKADDR_IN client_addr;
-    SOCKET client_socket = accept(server_socket, (SOCKADDR*)&client_addr, &sockaddr_size);
-    if (client_socket == -1)
-        std::cout << "accept errno : " << errno << std::endl;
 
-    const std::string dst_path = "C:/Users/mnt/Desktop/dd";
+    while (!_kbhit())
+    {
+        SOCKET client_socket = accept(server_socket, (SOCKADDR*)&client_addr, &sockaddr_size);
+        if (client_socket == -1)
+        {
+            std::cout << "can't accept client_socket : " << std::endl;
+            continue;
+        }
 
-    if (IsExistDiretory(dst_path) == false)
-        return false;
+        char dst_path[PACKET_SIZE] = { 0 };
+        int recvlen = recv(client_socket, dst_path, PACKET_SIZE, 0);
+        if (recvlen != sizeof(dst_path))
+        {
+            std::cout << "can't recv dst_path : " << std::endl;
+            continue;
+        }
+        std::cout << "dst_path : " << dst_path << std::endl;
 
-    DirectoryCopy(dst_path, client_socket);
-    
+        if (IsExistDirectory(dst_path) == false)
+        {
+            std::cout << "can't check Directory" << std::endl;
+            continue;
+        }
+
+        if (DirectoryCopy(dst_path, client_socket) == false)
+        {
+            std::cout << "fail to copy directory" << std::endl;
+            continue;
+        }
+
+        closesocket(client_socket);
+
+        std::cout << "end file copy" << std::endl;
+
+    }
+    _getch();
     closesocket(server_socket);
-    closesocket(client_socket);
 
     WSACleanup();
     return true;
