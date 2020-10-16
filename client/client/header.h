7a71ca4c1b46ec402679351c068baa3008cf7506
mnt907 <mnt907@naver.com>
2020-10-16 10:27:12 +0900

    add header.h

---
+++ client/client/header.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <csignal>
+#include <iostream>
+#include <sys/types.h>
+#include <string.h>
+#include <thread>
+#include <condition_variable>
+#include <map>
+#include <queue>
+#include <fstream>
+#include <istream>
+#include <mutex>
+#include <string>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>  
+#include <signal.h>  
+#include <locale>
+#ifdef _WIN32
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <conio.h>
+#include <WS2tcpip.h>
+#include <filesystem>
+#include <direct.h>
+#else
+#include <sys/time.h> 
+#include <sys/select.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h> 
+#include <unistd.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <iconv.h>
+#endif // _WIN32
+
+struct Header
+{
+	enum class TYPE
+	{
+		CONNECT,
+		CHECK_DIR,
+		NOT_EXIST_DIR,
+		ERROR_SIG
+	};
+
+	TYPE type;
+	int64_t length;
+	bool is_dir;
+	char file_path[256];
+	//struct stat file_info;
+
+	Header()
+		: type(Header::TYPE::CONNECT)
+		, length(0)
+		, is_dir(false)
+		//, file_info{}
+	{
+		memset(file_path, 0, sizeof(file_path));
+	}
+};
